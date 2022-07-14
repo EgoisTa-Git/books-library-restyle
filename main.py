8d920d149c9389ed4f8f994dfab98ae62d410fad
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-07-14 23:27:12 +0300

    add ConnectionError protection to parser

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
@@ -72,43 +73,57 @@ def parse_arguments():
     return start, end
 
 
+def get_book_response(url, book_id):
+    payload = {
+        'id': book_id
+    }
+    response = requests.get(url, params=payload, allow_redirects=False)
+    response.raise_for_status()
+    return response
+
+
+def get_book_page_response(page_url):
+    page_response = requests.get(page_url)
+    page_response.raise_for_status()
+    return page_response
+
+
 if __name__ == '__main__':
     os.makedirs(BOOK_DIR, exist_ok=True)
     os.makedirs(IMAGE_DIR, exist_ok=True)
     start_id, end_id = parse_arguments()
     for id_ in range(start_id, end_id):
-        payload = {
-            'id': id_
-        }
         book_url = f'https://tululu.org/txt.php'
-        book_response = requests.get(
-            book_url,
-            params=payload,
-            allow_redirects=False,
-        )
-        try:
-            book_response.raise_for_status()
-        except requests.HTTPError:
-            print('Errors on the client or server side')
-            continue
-        except requests.ConnectionError:
-            print('Connection error occurs! Trying next book...')
-            continue
+        connection = False
+        while not connection:
+            try:
+                print(f'Trying to get book #{id_}')
+                book_response = get_book_response(book_url, id_)
+                connection = True
+            except requests.HTTPError:
+                print('Errors on the client or server side')
+                continue
+            except requests.ConnectionError:
+                print('Connection error occurs! Trying to get book...')
+                sleep(5)
         try:
             check_for_redirect(book_response)
         except requests.TooManyRedirects:
             print(f'There isn`t book with ID {id_}. Redirect detected!')
             continue
         book_page_url = f'https://tululu.org/b{id_}/'
-        book_page_response = requests.get(book_page_url)
-        try:
-            book_page_response.raise_for_status()
-        except requests.HTTPError:
-            print('Errors on the client or server side')
-            continue
-        except requests.ConnectionError:
-            print('Connection error occurs! Trying next book...')
-            continue
+        connection = False
+        while not connection:
+            try:
+                print(f'Trying to get #{id_} book properties')
+                book_page_response = get_book_page_response(book_page_url)
+                connection = True
+            except requests.HTTPError:
+                print('Errors on the client or server side')
+                continue
+            except requests.ConnectionError:
+                print('Connection error occurs! Trying to get book...')
+                sleep(5)
         try:
             check_for_redirect(book_page_response)
         except requests.TooManyRedirects:
